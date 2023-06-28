e77cc0b90c9faf547a2edf62483c5af3370c3cf0
serhii.poliarush <cedjey@gmail.com>
2023-06-28 18:37:25 +0300

    save

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-// Material Dashboard 2 React Context Provider
 import { MaterialUIControllerProvider } from './mdb/context';
+import App from './App';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
