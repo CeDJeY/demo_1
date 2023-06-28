f2d937c2b72fe055b9b889d40ae856ea1fea12a1
serhii.poliarush <cedjey@gmail.com>
2023-06-28 15:52:48 +0300

    fix UI components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
 
+import CssBaseline from "@mui/material/CssBaseline";
+import Sidenav from "./mdb/components/Sidenav";
+// import routes from "../material-dashboard/src/routes";
+import Configurator from "../material-dashboard/src/examples/Configurator";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+
+import theme from './mdb/assets/theme';
+import { useMaterialUIController } from './mdb/context';
+
+// Images
+import brandWhite from './mdb/assets/images/logo-ct.png';
+import brandDark from './mdb/assets/images/logo-ct-dark.png';
+
 
 function App() {
+  const [ controller, dispatch ] = useMaterialUIController();
+  const {
+    miniSidenav,
+    direction,
+    layout,
+    openConfigurator,
+    sidenavColor,
+    transparentSidenav,
+    whiteSidenav,
+    darkMode,
+  } = controller;
+
   return (
-    <div className="App">
-      123
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Sidenav
+        color={sidenavColor}
+        brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+        brandName="Material Dashboard 2"
+        routes={[]}
+        onMouseEnter={() => {}}
+        onMouseLeave={() => {}}
+      />
+      {/*{layout === "vr" && <Configurator />}*/}
+      {/*<Routes>*/}
+      {/*  {getRoutes(routes)}*/}
+      {/*  <Route path="*" element={<Navigate to="/dashboard" />} />*/}
+      {/*</Routes>*/}
+      1234
+    </ThemeProvider>
   );
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+// Material Dashboard 2 React Context Provider
+import { MaterialUIControllerProvider } from './mdb/context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <MaterialUIControllerProvider>
+        <App />
+      </MaterialUIControllerProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
