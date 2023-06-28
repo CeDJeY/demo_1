a6c5f1a642acef0d099d4ba45ff89e5527d6203c
serhii.poliarush <cedjey@gmail.com>
2023-06-28 18:22:53 +0300

    add fonts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,67 +1,33 @@
 import React, { useState } from 'react';
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Sidenav from './mdb/components/Sidenav';
-import routes from './routes';
-// import Configurator from "./mdb/examples/Configurator";
-import { Navigate, Route, Routes } from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-
 import theme from './mdb/assets/theme';
-import { useMaterialUIController, setMiniSidenav } from './mdb/context';
-
-// Images
 import brandWhite from './mdb/assets/images/logo-ct.png';
-import brandDark from './mdb/assets/images/logo-ct-dark.png';
+import { useMaterialUIController } from './mdb/context';
+import routes from './routes';
 
 
 function App() {
   const [ controller, dispatch ] = useMaterialUIController();
-  const {
-    miniSidenav,
-    direction,
-    layout,
-    openConfigurator,
-    sidenavColor,
-    transparentSidenav,
-    whiteSidenav,
-    darkMode,
-  } = controller;
-  const [onMouseEnter, setOnMouseEnter] = useState(false);
-
-  // Open sidenav when mouse enter on mini sidenav
-  const handleOnMouseEnter = () => {
-    if (miniSidenav && !onMouseEnter) {
-      setMiniSidenav(dispatch, false);
-      setOnMouseEnter(true);
-    }
-  };
-
-  // Close sidenav when mouse leave mini sidenav
-  const handleOnMouseLeave = () => {
-    if (onMouseEnter) {
-      setMiniSidenav(dispatch, true);
-      setOnMouseEnter(false);
-    }
-  };
+  const { sidenavColor } = controller;
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Sidenav
         color={sidenavColor}
-        brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
+        brand={brandWhite}
         brandName="Assignments"
         routes={routes}
-        onMouseEnter={handleOnMouseEnter}
-        onMouseLeave={handleOnMouseLeave}
       />
-      {/*{layout === "vr" && <Configurator />}*/}
-      {/*<Routes>*/}
-      {/*  {getRoutes(routes)}*/}
-      {/*  <Route path="*" element={<Navigate to="/dashboard" />} />*/}
-      {/*</Routes>*/}
-
+      <Routes>
+        {routes.map((route, index) => {
+          return <Route key={index} path={route.route} element={route.component} />
+        })}
+        <Route path="*" element={<Navigate to="/dashboard" />} />
+      </Routes>
     </ThemeProvider>
   );
 }
